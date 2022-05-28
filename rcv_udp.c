02656e16fad74939453405a50a763e37f97f0ac2
John Ziamos <iziamos@gmail.com>
2022-05-28 13:29:53 +0100

    megacommit -- change seq number in the payload, check that its in order and slow down flow

--- rcv_udp.c
+++ rcv_udp.c
@@ -4,15 +4,34 @@
 #include <stdio.h>
 
 #define PORT 10001
-#define BUFFER_LENGTH 512
+#define BUFFER_LENGTH 64
+
+unsigned long
+read_ul_from_buffer(unsigned char * buffer) {
+
+    unsigned long ret = 0l;
+
+    ret |= (buffer[0] << 24) ;
+    ret |= (buffer[1] << 16) ;
+    ret |= (buffer[2] <<  8) ;
+    ret |= (buffer[3]) ;
+
+    return ret;
+}
+
+int
+assert_seq_in_order(unsigned long previous, unsigned long current) {
+    return (current == 0l) || (previous == -1) || ((previous + 1) == current);
+}
 
 int
 main (int argv, char ** argc) {
 
     int fd, n;
     struct sockaddr_in address;
-
     char buffer[BUFFER_LENGTH];
+
+    unsigned long previous = -1l;
 
     if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         printf("Failed to create socket");
@@ -29,11 +48,29 @@ main (int argv, char ** argc) {
         return -1;
     }
 
+    int faults = 0;
+
     for(;;) {
-        n = recv(fd, buffer, BUFFER_LENGTH, MSG_WAITFORONE);
+        n = recv(fd, buffer, BUFFER_LENGTH, MSG_DONTWAIT);
         if (n > 0) {
             buffer[n] = '\0';
+            unsigned long seq = read_ul_from_buffer(buffer);
+            printf("Seq number: '%ul'\n", seq);
+
+            if (assert_seq_in_order(previous, seq))
+            {
+                previous = seq;
+                continue;
+            }
+
+            printf("Expected sequence %ul but got %ul this is the %d fault!\n", previous + 1, seq, faults);
+            previous = seq;
+            faults++;
+
+            if (faults > 10) {
+                return -1;
+            }
         }
-        printf("%s\n", buffer);
+
     }
 }
--- snd_udp.c
+++ snd_udp.c
@@ -3,18 +3,29 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define PORT 10001
 #define BUFFER_LENGTH 64
+
+void
+write_ul_to_buffer(unsigned char * buffer, unsigned long l) {
+    buffer[0] = (l >> 24) & 0xFF;
+    buffer[1] = (l >> 16) & 0xFF;
+    buffer[2] = (l >>  8) & 0xFF;
+    buffer[3] =  l        & 0xFF;
+}
 
 int
 main (int argv, char ** argc) {
 
     int fd;
     struct sockaddr_in address;
-
     char buffer[BUFFER_LENGTH];
-    char * blah = "bhal";
+
+    struct timespec pause;
+    pause.tv_sec = 0;
+    pause.tv_nsec = 1000 * 1000 * 1000;
 
     if ((fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         printf("Failed to create socket\n");
@@ -26,13 +37,11 @@ main (int argv, char ** argc) {
     // address.sin_addr.s_addr = inet_addr("239.193.4.0");
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    strcpy(buffer, "Seq number: ");
-
-    // len12
-
     for(unsigned long i = 0; ; ++i) {
-        sprintf(buffer + 12, "%ul", i);
+        write_ul_to_buffer(buffer, i);
 
-        sendto(fd, buffer, 57, 0, (struct sockaddr *)&address, sizeof(address));
+        //send the whole buffer
+        sendto(fd, buffer, BUFFER_LENGTH, 0, (struct sockaddr *)&address, sizeof(address));
+        nanosleep(&pause, NULL);
     }
 }
